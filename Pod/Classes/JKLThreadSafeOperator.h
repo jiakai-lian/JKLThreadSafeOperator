f9bddf192f937852f56bf5e939aa1900798c34d3
jiakai lian <jiakai.lian@gmail.com>
2015-12-03 21:55:53 +1100

    Create JKLTHreadSafeOperator class

---
+++ Pod/Classes/JKLThreadSafeOperator.h
@@ -0,0 +1,13 @@
+//
+//  JKLThreadSafeOperator.h
+//  Pods
+//
+//  Created by jiakai lian on 3/12/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKLThreadSafeOperator : NSObject
+
+@end
---
+++ Pod/Classes/JKLThreadSafeOperator.m
@@ -0,0 +1,13 @@
+//
+//  JKLThreadSafeOperator.m
+//  Pods
+//
+//  Created by jiakai lian on 3/12/2015.
+//
+//
+
+#import "JKLThreadSafeOperator.h"
+
+@implementation JKLThreadSafeOperator
+
+@end
